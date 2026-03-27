4c9f3dca8dd70526440448e167b5f34275066864
Robert Moraru <robert.moraru@Bogdan-Stans-MacBook-Pro.local>
2026-03-27 16:50:10 +0200

    3rd commit ui fixes

---
+++ tests/Browser/RegisterTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+
+test('registers a user', function () {
+    visit('/register')
+        ->fill('name','John Doe')
+        ->fill('email',"john@example.com")
+        ->fill('password', 'password123!@')
+        ->click('Create Account')
+
+        ->assertPathIs('/');
+
+    $this->assertAuthenticated();
+
+    expect(Auth::user())->toMatchArray([
+        'name' => 'John Doe',
+        'email' => "john@example.com"
+    ]);
+});
